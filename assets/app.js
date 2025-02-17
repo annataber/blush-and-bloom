5125c0d191a34f2d3b50c8b9eed0030b43732fd5
Ben Taber <ben.taber@gmail.com>
2025-02-16 18:26:54 -0700

    cleanup and some fun fading 🧹

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
 const products = [];
 
 async function fetchDataFromSheets() {
-  // const sheetsApi = 'https://script.google.com/macros/s/AKfycbyGqpVCNN0dCBchOdZlrJ6Ap_EHG5TP_WF4i9YEXOw4vWMg5OwR5BiNrPYhFhp9dQh0/exec';
+  const sheetsApi = 'https://script.google.com/macros/s/AKfycbyGqpVCNN0dCBchOdZlrJ6Ap_EHG5TP_WF4i9YEXOw4vWMg5OwR5BiNrPYhFhp9dQh0/exec';
 
   // for faster testing
-  const sheetsApi = '/assets/products.json';
+  // const sheetsApi = '/assets/products.json';
 
   const response = await fetch(sheetsApi);
   (await response.json()).forEach((product, index) => {
@@ -16,13 +16,15 @@ async function fetchDataFromSheets() {
         .filter((value) => !!value); // remove any empty values
     });
 
-    products.push(product);
+    if (product.product && product.description && product.image && product.link && product.price) {
+      products.push(product);
+    }
   });
 
   console.log(products);
 }
 
-function storeData(dataType, element) {
+function storeChoice(dataType, element) {
   // remove selected from any previously chosen item
   const items = document.querySelectorAll(`#view-${dataType} li`);
   Array.from(items).forEach((item) => {
@@ -37,12 +39,19 @@ function storeData(dataType, element) {
   window.localStorage.setItem(dataType, Number(choice));
 }
 
-function loadData(dataType) {
+function loadChoice(dataType) {
   const choice = window.localStorage.getItem(dataType);
 
   if (choice) {
-    storeData(dataType, document.querySelector(`#view-${dataType} li[data-choice='${choice}']`));
+    storeChoice(dataType, document.querySelector(`#view-${dataType} li[data-choice='${choice}']`));
   }
+}
+
+function clearChoices() {
+  window.localStorage.clear();
+  document.querySelectorAll('li.selected').forEach((element) => {
+    element.classList.remove('selected');
+  });
 }
 
 async function fade(element, targetOpacity) {
@@ -87,17 +96,16 @@ function findMatchingProducts() {
   const style = window.localStorage.getItem('style');
 
   const ranges = {
-    '1': [0, 15],
-    '2': [15, 25],
-    '3': [25, 50],
-    '4': [50, 1000],
+    '1': [0, 25],
+    '2': [25, 50],
+    '3': [50, 75],
+    '4': [75, 1000],
   };
 
   console.log('stored data', { eyeColor, hairColor, skinTone, priceRange, style });
 
   // filter products based on stored data
   return products.filter((product) => {
-    console.log('filtering', product);
     if (eyeColor && product.eye_colors.length && !product.eye_colors.includes(eyeColor)) {
       return false;
     }
@@ -114,8 +122,10 @@ function findMatchingProducts() {
       return false;
     }
 
-    if (priceRange && product.price && !(ranges[priceRange][0] <= product.price <= ranges[priceRange][1])) {
-      return false;
+    if (priceRange && product.price) {
+      const min = ranges[priceRange][0];
+      const max = ranges[priceRange][1];
+      return product.price >= min && product.price < max;
     }
 
     return true;
@@ -123,19 +133,25 @@ function findMatchingProducts() {
 }
 
 function buildProductList() {
+  const list = document.querySelector('#product-list');
   const matchingProducts = findMatchingProducts();
   console.log('matchingProducts', matchingProducts);
 
-  document.querySelector('#product-list').innerHTML = matchingProducts.map((product) => {
-    return `
-      <a href="${product.link}" target="_blank">
-        <div class="product-image" style="background-image: url(${product.image})"></div>
-        <div class="product-details">
-          <h3>${product.product}</h3>
-          <p>${product.description}</p>
-          <span class="price">$${product.price}.00</span>
-        </div>
-      </a>
-    `;
-  }).join('');
+  if (!matchingProducts.length) {
+    list.innerHTML = `<p class="sorry">Sorry, no makeup found! Try changing your choices.</p>`;
+  }
+  else {
+    list.innerHTML = matchingProducts.map((product) => {
+      return `
+        <a href="${product.link}" target="_blank">
+          <div class="product-image" style="background-image: url(${product.image})"></div>
+          <div class="product-details">
+            <h3>${product.product}</h3>
+            <p>${product.description}</p>
+            <span class="price">$${product.price}.00</span>
+          </div>
+        </a>
+      `;
+    }).join('');
+  }
 }
